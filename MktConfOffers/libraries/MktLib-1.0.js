d1a6c853433095d3c32d5b25ab98c592fafb13a7
Val Huber <val.huber@ca.com>
2018-05-06 18:16:03 -0700

    Proper checkin

---
+++ MktConfOffers/libraries/MktLib-1.0.js
@@ -0,0 +1,4 @@
+var Conf = {    // preferably, load values from property files, as in MktMgmt
+    acctgURL: 'http://localhost:8080/rest/default/MktMgmt/v1/ProcessCharges',
+    acctgAuth: { headers: { Authorization: "CALiveAPICreator AcctgToken:1" }}
+};
